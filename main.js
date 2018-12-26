67453dd5de63b36901c37d6cb45bd6e31373a17d
Josiah Haswell <josiah.haswell@gmail.com>
2018-12-26 16:31:03 -0700

    Initial Import

---
+++ main.js
@@ -0,0 +1,8 @@
+
+const paths = require('./src/main/paths.js'),
+    buildscripts = require('./src/main/tasks/build.js'),
+    cleanscripts = require('./src/main/tasks/clean.js'),
+    servescripts = require('./src/main/tasks/serve.js'),
+    sitescripts = require('./src/main/tasks/site.js'),
+    utilscripts = require('./src/main/tasks/utils.js'),
+    watchscripts = require('./src/main/tasks/watch.js');
